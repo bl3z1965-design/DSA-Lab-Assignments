4ce7199de1d7e440d198601addb73e08c7fddd76
Tushar <bl3z1965@gmail.com>
2026-08-09 17:02:04 +0530

    Commit

---
+++ rotate.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+#define MAX_SIZE 100
+
+int main(){
+    int arr[MAX_SIZE], n, k;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cout << "Enter Element " << i+1 << ": ";
+        cin >> arr[i];
+    }
+    cout << "Enter how many positions you want to shift the array: ";
+    cin >> k;
+    k = k % n;
+    cout << "Original Array: ";
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    for(int i = 0; i < k; i++){
+        int temp = arr[0];
+        for(int j = 1; j < n; j++){
+            arr[j - 1] = arr[j];
+        }
+        arr[n-1] = temp;
+    }
+    cout << "Rotated Array: ";
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
