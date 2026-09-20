368cab8b0c4ae75c3b7ab4af59e2fb2bc32d2139
Tushar <bl3z1965@gmail.com>
2026-09-20 19:24:23 +0530

    Commit

--- Assignment-04/A4_Q5.cpp
+++ Assignment-04/A4_Q5.cpp
@@ -6,12 +6,13 @@ using namespace std;
 class Queue{
 
     private: 
-        char arr[256];
+        int arr[256];
         int front;
         int rear;
+        int count;
 
     public:
-        Queue() : front(-1), rear(-1) {}
+        Queue() : front(-1), rear(-1), count(0) {}
 
         bool isEmpty(){
             return(front == -1 || front > rear);
@@ -21,7 +22,7 @@ class Queue{
             return(rear == 255);
         }
 
-        void enqueue(char value){
+        void enqueue(int value){
             if(isFull())
                 return;
             
@@ -30,22 +31,29 @@ class Queue{
             
             rear++;
             arr[rear] = value;
+            count++;
         }
 
-        void dequeue(){
+        int dequeue(){
             if(isEmpty())
-                return;
-            
+                return -1;
+            int temp = arr[front];
             front++;
 
             if(front > rear){
                 front = -1;
                 rear = -1;
             }
+            count--;
+            return temp;
         }
 
-        char peek(){
+        int peek(){
             return arr[front];
+        }
+
+        int size(){
+            return count;
         }
 };
 
@@ -54,8 +62,90 @@ class StackTwoQueue{
         Queue q1, q2;
 
     public:
-        void push(){
-            
+        void push(int value){
+            if(q1.isFull()){
+                cout << "Stack Overflow" << endl;
+                return;
+            }
+            q2.enqueue(value);
+            while(!q1.isEmpty()){
+                q2.enqueue(q1.dequeue());
+            }
+            Queue temp = q2;
+            q2 = q1;
+            q1 = temp;
         }
 
+        int pop(){
+            if(q1.isEmpty()){
+                cout << "Stack Undeflow" << endl;
+                return -1;
+            }
+            return q1.dequeue();
+        }
+
+        int top(){
+            if(q1.isEmpty()){
+                cout << "Stack Undeflow" << endl;
+                return -1;
+            }
+            return q1.peek();
+        }
 };
+
+class StackOneQueue{
+    private:
+        Queue q;
+
+    public:
+        void push(int value){
+            if(q.isFull()){
+                cout << "Stack Overflow" << endl;
+                return;
+            }
+            int n = q.size();
+            q.enqueue(value);
+            for(int i = 0; i < n; i++)
+                q.enqueue(q.dequeue());
+        }
+
+        int pop(){
+            if(q.isEmpty()){
+                cout << "Stack Undeflow" << endl;
+                return -1;
+            }
+            return q.dequeue();
+        }
+
+        int top(){
+            if(q.isEmpty()){
+                cout << "Stack Undeflow" << endl;
+                return -1;
+            }
+            return q.peek();
+        }
+};
+
+int main() {
+    cout << "--- Stack using TWO queues ---\n";
+    StackTwoQueue s1;
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+    cout << "Top: " << s1.top() << endl;   // 30
+    cout << "Pop: " << s1.pop() << endl;   // 30
+    cout << "Pop: " << s1.pop() << endl;   // 20
+    cout << "Top: " << s1.top() << endl;   // 10
+
+    cout << "\n--- Stack using ONE queue ---\n";
+    StackOneQueue s2;
+    s2.push(10);
+    s2.push(20);
+    s2.push(30);
+    cout << "Top: " << s2.top() << endl;   // 30
+    cout << "Pop: " << s2.pop() << endl;   // 30
+    cout << "Pop: " << s2.pop() << endl;   // 20
+    cout << "Top: " << s2.top() << endl;   // 10
+
+    return 0;
+}
